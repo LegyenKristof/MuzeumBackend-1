c6f3299cbff7b9c7c06ce67cc9c292f623d9bd47
Your Name <you@example.com>
2022-02-02 11:59:49 +0100

    Layout és componens kész

--- resources/views/statues/create.blade.php
+++ resources/views/statues/create.blade.php
@@ -1,51 +1,41 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="{{ mix('css/bootstrap.css') }}">
-    <link rel="stylesheet" href="{{ mix('css/app.css') }}">
-    <link rel="stylesheet" href="{{ mix('css/masik.css') }}">
-    <title>Document</title>
-</head>
-<body>
-    <h1>New statue</h1>
+@extends('layout')
 
-    <!-- Összes hiba egy helyen -->
+@section('content')
+<h1>New statue</h1>
 
-    @if ($errors->any())
-        @foreach ($errors->all() as $error)
-            <p>{{ $error }}</p>
-        @endforeach
-    @endif
+<!-- Összes hiba egy helyen -->
 
-    <form method='POST' action="{{ route('statues.store') }}">
-        <input type="hidden" name="_token" value="{{ csrf_token() }}">
-        <div>
-            Person:<br>
-            <input type="text" name="person" value="{{ old('person') }}">
-            @error('person')
-                <p>{{ $message }}</p>
-            @enderror
-        </div>
-        <div>
-            Height:<br>
-            <input type="number" name="height" value="{{ old('height') }}">
-            @error('height')
-                <p>{{ $message }}</p>
-            @enderror
-        </div>
-        <div>
-            Price:<br>
-            <input type="number" name="price" value="{{ old('price') }}">
-            @error('price')
-                <p>{{ $message }}</p>
-            @enderror
-        </div>
-        <div>
-            <input type="submit" value="Create" class="btn btn-primary">
-        </div>
-    </form>
-</body>
-</html>
+@if ($errors->any())
+    @foreach ($errors->all() as $error)
+        <p>{{ $error }}</p>
+    @endforeach
+@endif
+
+<form method='POST' action="{{ route('statues.store') }}">
+    <input type="hidden" name="_token" value="{{ csrf_token() }}">
+    <div>
+        Person:<br>
+        <input type="text" name="person" value="{{ old('person') }}">
+        @error('person')
+            <p>{{ $message }}</p>
+        @enderror
+    </div>
+    <div>
+        Height:<br>
+        <input type="number" name="height" value="{{ old('height') }}">
+        @error('height')
+            <p>{{ $message }}</p>
+        @enderror
+    </div>
+    <div>
+        Price:<br>
+        <input type="number" name="price" value="{{ old('price') }}">
+        @error('price')
+            <p>{{ $message }}</p>
+        @enderror
+    </div>
+    <div>
+        <input type="submit" value="Create" class="btn btn-primary">
+    </div>
+</form>
+@endsection
